59bce7a2d0f66f44a6f5e77d7ced654c89b78531
Ed-Maron <lazycoast102@gmail.com>
2019-05-18 15:02:29 +0400

    Кнопка создания курса

--- serverAPI/coursesAPI.js
+++ serverAPI/coursesAPI.js
@@ -21,5 +21,15 @@ module.exports = function(app, knex){
                 errorMsg
             });
         }
-	});
+    });
+    
+    app.get('/courseAPI/lastCourseId', async(req, res)=>{
+        try {
+            let lastCourseId = await knex("Courses").max("id");
+		    res.send(lastCourseId);
+        } catch (error) {
+            console.error(error.ToString());
+            res.status(404).end();
+        }
+    });
 };
--- src/blocks/header.js
+++ src/blocks/header.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import Settings from '../../dist/settings.svg';
 import Search from '../../dist/search.svg';
-
+import { Link } from 'react-router-dom';
+import {URL} from '../consts';
 class Header extends React.Component {
 	constructor(props){
         super(props);
+        this.state={
+            lastCourseId: -1
+        }
         this.onLogout = this.onLogout.bind(this);
     }
     
-    async onLogout(e)  {
+    onLogout(e)  {
 		e.preventDefault();
 		try {
-			await this.props.logOut();
+			this.props.logOut();
 		}catch (error) {
 			console.log(`Error: ${error}`);
+		}
+    }
+    async componentDidMount() {
+		try {
+			let lastCourseIdRequest = await fetch(`${URL}/courseAPI/lastCourseId`);
+            const lastCourseIdJson = await lastCourseIdRequest.json();
+            let lastCourseId = lastCourseIdJson[0]["max(`id`)"] + 1;
+            this.setState({lastCourseId}); 
+		} catch (e) {
+			console.log('Error: ', e);
 		}
     }
     
@@ -21,6 +35,12 @@ class Header extends React.Component {
         let btnLogoutAndSettings = this.props.isAuth ? 
                             (
                                 <React.Fragment>
+                                    <Link to={"/editCourse/" + this.state.lastCourseId}>
+                                        <button type='button' onClick>
+                                            Создать курс
+                                        </button>
+                                    </Link>
+                                                            
                                     <button className='header__settings' type='button'>
                                         <img src={Settings} alt="" />
                                     </button>
