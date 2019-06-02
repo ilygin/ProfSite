7e99afbeabc50b180fed2f100abc75ebc66118de
Ed-Maron <lazycoast102@gmail.com>
2019-06-02 21:04:00 +0400

    draft wysiwyg

--- src/containers/editCoursePage.js
+++ src/containers/editCoursePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from '../blocks/sidebar/sidebar';
-import Content from '../blocks/tableContentsContent';
+import TableContent from '../blocks/tableContentsContent';
+import EditCoursePage from '../blocks/editCoursePageContent';
 import {connect} from 'react-redux';
 import * as saveCourseActions from '../actions/saveCourseActions';
 import * as userActions from '../actions/userActions';
@@ -12,15 +13,22 @@ class TableContentsPage extends React.Component {
 
 	
 	render() {
+		console.group(`this.props.match.params:`);
+			console.log(this.props.match);
+		console.groupEnd(`this.props.match.params:`);
+
 		const {editCourseStatus, logOutUser, saveCourseChange, tableContent} = this.props;
+		const PageContent = this.props.match.path.includes("/edit_course/") ? 
+			<TableContent courseId = {this.props.match.params.id}
+				editCourseStatus = {editCourseStatus}
+				saveCourseChange = {saveCourseChange}
+				tableContent = {tableContent}
+				logOut={logOutUser}/> :
+			<EditCoursePage />;
 		return (
 			<div className={'main_page'}>
 				<Sidebar />
-				<Content courseId = {this.props.match.params.id}
-						 editCourseStatus = {editCourseStatus}
-						 saveCourseChange = {saveCourseChange}
-						 tableContent = {tableContent}
-						 logOut={logOutUser}/>
+				{PageContent}
 			</div>
 		)
 	}
